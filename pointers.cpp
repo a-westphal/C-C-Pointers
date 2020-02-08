ba46d2616ad876e35c3a2f99a205cceda4693338
Abby Westphal <47372310+a-westphal@users.noreply.github.com>
2020-02-07 23:12:21 -0600

    Add files via upload
    Fix the error checking for assignments

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,7 @@
 // compile: g++ -std=c++11 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <math.h>
 
 typedef struct Student {
     int id;
@@ -15,16 +16,18 @@ void calculateStudentAverage(void *object, double *avg);
 int main(int argc, char **argv)
 {
     Student *student;
-    double average;
+    double average = 0;
 
     //holder variables for the strings:
     int in_id;          //inputted id number
     char *first;        //inputted first name
     char *last;         //inputted last name
-    int assignments;    //inputted assignment number
+    char *assignments;    //inputted assignment number
+    int assign;
 
     first = (char*)malloc((129)*sizeof(char));
     last = (char*)malloc((129)*sizeof(char));
+    assignments = (char*)malloc((129)*sizeof(char));
 
     // Sequence of user input -> store in fields of `student`
     //Student id: 
@@ -43,35 +46,39 @@ int main(int argc, char **argv)
     student->l_name = last;
 
     //Number of Assignments:
+    /* store the assignments as a string or array of chars to be checked 
+        for letters */
     bool ask = true;
+    bool check = true;
+    int index = 0;
+
+
+
     while(ask)
     {
         std::cout << "Please enter how many assignments were graded: ";
         std::cin >> assignments;
-       // string assign_check = std::to_string(assignments);
-        //std::cout <<" assign check, alpha output" << all(assignments,isalpha());
-
-       /*if(std::cin.fail())
-        {
-            std::cout << "Sorry, I cannot understand your answer\n";
-        }  */ 
-        if(assignments < 1)
+        std::cout << " assignments: " << assignments;
+        
+        if(atoi(assignments) < 1)
         {
             std::cout << "Sorry, I cannot understand your answer\n";
         }
         else
         {
             ask = false;
+            assign = atoi(assignments);
+
         }
     }
     
-    student->n_assignments = assignments; 
+    student->n_assignments = assign; 
     std::cout <<"\n";
 
     //Ask for grades: 
-    student->grades = new double [assignments];
+    student->grades = new double [assign];
     double grade;
-    for(int i = 0; i < assignments; i ++)
+    for(int i = 0; i < assign; i ++)
     {
         bool reask = true; 
         std::cout << "Please enter grade for assignment " << i << ": ";
@@ -79,7 +86,7 @@ int main(int argc, char **argv)
 
         while(reask)
         {
-            if(grade > 100.00 || grade <= 0)
+            if(grade > 1000.00 || grade <= 0)
             {
                 std::cout << "Sorry, I cannot understand your answer\n";
                 std::cout << "Please enter grade for assignment " << i << ": ";
@@ -94,28 +101,32 @@ int main(int argc, char **argv)
         student->grades[i] = grade;
     }    
 
-    // Call `CalculateStudentAverage(???, &average)`
-    calculateStudentAverage(student->grades,&average);
-    // Output `average`
+    //call to the average:
+    calculateStudentAverage(student,&average);
+
+    std::cout << "\nStudent: " << student->f_name << " " << student->l_name << " [" << student->id << "]" <<std::endl;
+    std::cout <<"  Average grade: " << average <<std::endl;
 
     return 0;
 }
 
 void calculateStudentAverage(void *object, double *avg)
 {
-    double calc; 
+    double add = 0;
+    Student *hold; 
+
+    //used for rounding: 
+    double scale = 0.1;
+
+
+    //need to cast the object back to what it was: 
+    hold = (Student*)(object);
     
-    std::cout << "size of object: " << sizeof(object) << "\n";
+    for(int i =0; i< hold->n_assignments; i ++)
+    {
+        add += hold->grades[i];
+    } 
 
-    // Code to calculate and store average grade
-
-    //when the average is calculated, we are editing the average value
-    //that is passed, which is the double average defined within main
-    //rather than returning a value to be stored within the variable 
-    //in main
-
-
-
-    //post math = 
-    // *avg = calc;
+    *avg = add / hold->n_assignments;
+    *avg = floor(*avg / scale + 0.5) * scale;
 }
